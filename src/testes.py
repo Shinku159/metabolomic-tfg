6651be9195bd0e1d16ff116831e352eedd779d5f
Shinku159 <shinku.itajuba@outlook.com>
2022-10-24 19:29:23 -0300

    teste

--- src/testes.py
+++ src/testes.py
@@ -1,17 +1,11 @@
-
-from sklearn.metrics import accuracy_score, classification_report
-from models.MlpModel import MlpModel
-import dataCollector as dc
 import tensorflow as tf
+import dataCollector
+from models.ConvModel import ConvModel
 
-x, xt, y, yt, inputShape, outputShape = dc.collect(0, 0) #collect dims data0base for mlp model 
+gpus = tf.config.list_physical_devices(device_type = 'GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
 
-model = MlpModel(inputShape, outputShape, metrics = ['accuracy'])
+x, xt, y, yt, inputShape, outputShape = dataCollector.collect(0, 1) #Base dims for conv model.
+
+model = ConvModel(inputShape, outputShape, metrics = ['accuracy'])
 model.fit(x, y, validation_split=0.3, epochs=200, batch_size=min(200, x.size), shuffle=True)
-
-yp = model.predict(xt)
-
-ac = accuracy_score(yt, yp.round())
-
-print(classification_report(yt, yp.round()))
-print("accuracy: {0}".format(ac))      
